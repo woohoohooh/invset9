70d2a73e7e74e58616166dea6dee36a06eb14d52
woohoohooh@gmail.com <woohoohooh@whatever.com>
2023-06-16 10:27:13 +0700

    commit2

--- myblog/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, Comment, Company, Advantage, Review
-
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Company)
-admin.site.register(Advantage)
-admin.site.register(Review)
